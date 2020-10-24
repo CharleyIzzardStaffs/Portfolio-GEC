3225376996485253bafc3353547b894648135e65
charley izzard <charleyizzard7@gmail.com>
2020-10-24 16:30:30 +0100

    Program three Reupload

---
+++ Program Three/Program Three.cpp
@@ -0,0 +1,29 @@
+// Program Three.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myInt1 = 5;
+    int myInt2 = 10;
+
+    cout << "value stored in myInt1 is " << myInt1 << endl;
+    cout << "value stored in myInt2 is " << myInt2 << endl;
+
+    float myFloat1 = 1.2f, myFloat2;
+    myFloat2 = 9.9f;
+
+    cout << "value stored in myFloat1 is " << myFloat1 << endl;
+    cout << "value stored in myFloat2 is " << myFloat2 << endl;
+
+
+    char myChar = 'a';
+
+    cout << "value stored in myChar is " << myChar << endl;
+
+    return 0;
+}
+    
+
