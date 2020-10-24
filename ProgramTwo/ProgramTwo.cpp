3ebcc360062adf4f7d16a6134c58a1030a4dc226
charley izzard <charleyizzard7@gmail.com>
2020-10-24 16:20:52 +0100

    program two

---
+++ ProgramTwo/ProgramTwo.cpp
@@ -0,0 +1,14 @@
+// ProgramTwo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+int main()
+{
+    std::cout << "Size of Char" << sizeof(char) << " Bytes" << std::endl;
+    std::cout << "Size of Int" << sizeof(int) << " Bytes" << std::endl;
+    std::cout << "Size of Short int" << sizeof(short int) << " Bytes" << std::endl;
+    std::cout << "Size of Long int" << sizeof(long int) << " Bytes" << std::endl;
+    std::cout << "Size of Float" << sizeof(float) << " Bytes" << std::endl;
+    std::cout << "Size of Double" << sizeof(double) << " Bytes" << std::endl;
+    std::cout << "Size of WideChar" << sizeof(wchar_t) << " Bytes" << std::endl;
+}
