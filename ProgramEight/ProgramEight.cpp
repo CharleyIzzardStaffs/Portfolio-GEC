5b5a57bf5e3ab643463c85b915b5eb8832ee8a15
charley izzard <charleyizzard7@gmail.com>
2020-10-24 17:23:00 +0100

    program Eight

---
+++ ProgramEight/ProgramEight.cpp
@@ -0,0 +1,13 @@
+// ProgramEight.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    const float pi = 3.14159;
+    float radius = 25;
+    float area = pi * radius * radius;
+    cout << "the area of a circle with a radius of " << radius << "is " << area << endl;
+}
+
