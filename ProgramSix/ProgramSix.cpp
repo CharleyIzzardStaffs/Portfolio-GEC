ce536b809022a0e70f01b459eb03180793e550dc
charley izzard <charleyizzard7@gmail.com>
2020-10-24 16:57:56 +0100

    ProgramSix

---
+++ ProgramSix/ProgramSix.cpp
@@ -0,0 +1,13 @@
+// ProgramSix.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int width = 25;
+	int height = 15;
+	int area = width * height;
+	cout << "the area of a " << width << " by " << height << " rectangle is " << area << endl;
+}
+
