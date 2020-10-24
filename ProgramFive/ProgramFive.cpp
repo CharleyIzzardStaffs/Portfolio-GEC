19c9f7beec77c5440a5f4ee5bfe192963f57bc05
charley izzard <charleyizzard7@gmail.com>
2020-10-24 16:49:25 +0100

    ProgramFive

---
+++ ProgramFive/ProgramFive.cpp
@@ -0,0 +1,30 @@
+// ProgramFive.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 1;
+
+    cout << "num = " << num << endl;
+    cout << "increment operator before num = " << ++num << endl;
+    cout << "num after = " << num << endl;
+
+    num = 1;
+    cout << "num = " << num << endl;
+    cout << "increment operator before num = " << ++num << endl;
+    cout << "num after = " << num << endl;
+ 
+    num = 1;
+    cout << "num = " << num << endl;
+    cout << "decrement operator before num = " << --num << endl;
+    cout << "num after = " << num << endl;
+
+    num = 1;
+    cout << "num = " << num << endl;
+    cout << "decrement operator before num = " << --num << endl;
+    cout << "num after = " << num << endl;
+
+    return 0;
+}
